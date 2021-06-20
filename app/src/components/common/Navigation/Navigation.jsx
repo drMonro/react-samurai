7eeff23980315e3dde2bf0ff3d2641f8a14a29a0
drMonro <Bestpower13>
2021-06-20 16:07:56 +0700

    React JS 20
    Использование NavLink из browser-router-dom

--- app/src/components/common/Navigation/Navigation.jsx
+++ app/src/components/common/Navigation/Navigation.jsx
@@ -1,14 +1,20 @@
 import styles from './Navigation.module.css';
+import {NavLink} from "react-router-dom";
 
 
 export const Navigation = () => {
     return <nav className={styles._}>
         <ul className={styles._list}>
-            <li><a href="/profile">Profile</a></li>
-            <li><a href="/dialogs">Messages</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Music</a></li>
-            <li><a href="#">Settings</a></li>
+            <li className={styles._linkItem}>
+                <NavLink to="/profile" activeClassName={styles._linkActive}>Profile</NavLink>
+            </li>
+            <li className={styles._linkItem}>
+                <NavLink to="/dialogs" activeClassName={styles._linkActive}>Messages</NavLink>
+            </li>
+
+            {/*<li><a href="#">News</a></li>*/}
+            {/*<li><a href="#">Music</a></li>*/}
+            {/*<li><a href="#">Settings</a></li>*/}
         </ul>
     </nav>
 }
