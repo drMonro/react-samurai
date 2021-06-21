a1bb2a610b55d91fd3cf8c1b83e38a0ad4e625d4
drMonro <Bestpower13>
2021-06-21 18:33:06 +0700

    React JS 24 Начало ввода Модели Выносим данные в сторону

--- app/src/components/Dialogs/DialogHistory/DialogHistory.jsx
+++ app/src/components/Dialogs/DialogHistory/DialogHistory.jsx
@@ -2,11 +2,17 @@ import styles from './DialogHistory.module.css';
 import DialogItem from "../DialogItem/DialogItem";
 
 export const DialogHistory = () => {
+    const messagesData = [
+        {message: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'},
+        {message: 'Lorem ipsum dolor sit amet.'},
+        {message: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'},
+    ]
+
     return (
         <ul className={styles._dialogHistory}>
-            <DialogItem name="Olga" message="Lorem ipsum dolor sit amet, consectetur adipisicing."/>
-            <DialogItem name="Me" message="Lorem ipsum dolor sit amet."/>
-            <DialogItem name="Olga" message="Lorem ipsum dolor sit amet, consectetur adipisicing."/>
+            <DialogItem name="Olga" message={messagesData[0].message}/>
+            <DialogItem name="Me" message={messagesData[1].message}/>
+            <DialogItem name="Olga" message={messagesData[2].message}/>
 
         </ul>
     )
--- app/src/components/Dialogs/Recipients/Recipients.jsx
+++ app/src/components/Dialogs/Recipients/Recipients.jsx
@@ -2,12 +2,19 @@ import styles from './Recipients.module.css';
 import Recipient from "../Recipient/Recipient";
 
 export const Recipients = () => {
+    const messagesData = [
+        {id: 1, name: 'Olga'},
+        {id: 2, name: 'Toha'},
+        {id: 3, name: 'Alex'},
+        {id: 4, name: 'Marta'},
+    ]
+
     return (
         <ul className={styles._recipientsList}>
-            <Recipient name="Olga" id="1"/>
-            <Recipient name="Toha" id="2"/>
-            <Recipient name="Alex" id="3"/>
-            <Recipient name="Marta" id="4"/>
+            <Recipient name={messagesData[0].name} id={messagesData[0].id}/>
+            <Recipient name={messagesData[1].name} id={messagesData[1].id}/>
+            <Recipient name={messagesData[2].name} id={messagesData[2].id}/>
+            <Recipient name={messagesData[3].name} id={messagesData[3].id}/>
         </ul>
     )
 }
