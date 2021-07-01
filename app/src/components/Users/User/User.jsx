66d48d3d2584990fa1fca0829103ebf9e5b00110
drMonro <Bestpower13>
2021-07-02 01:34:27 +0700

    React JS 50-51 Получаем список пользователей по сети. Адаптируем данные Axios

--- app/src/components/Users/User/User.jsx
+++ app/src/components/Users/User/User.jsx
@@ -1,6 +1,7 @@
 import styles from './User.module.css';
+import userAvatar from '../../../assets/images/user-avatar.jpg'
 
-export const User = ({photoUrl, isFollow, name, onFollow, id}) => {
+export const User = ({isFollow, name, onFollow, id, photos}) => {
     const followUser = () => {
         onFollow(id);
     };
@@ -9,9 +10,15 @@ export const User = ({photoUrl, isFollow, name, onFollow, id}) => {
         return isFollow ? 'Unfollow' : 'Follow';
     }
 
+    const isUrl = (photo) => {
+        return !!photo ? photo : userAvatar;
+    }
+
+
     return (
         <li>
-            <p><img className={styles._avatar} src={photoUrl} width="90px" height="90px" alt=""/></p>
+            <p>{name}</p>
+            <p><img className={styles._avatar} src={isUrl(photos.small)} width="90px" height="90px" alt=""/></p>
             <button onClick={followUser}>{getFollowTitle()}</button>
         </li>)
 }
--- app/src/components/Users/Users.jsx
+++ app/src/components/Users/Users.jsx
@@ -1,53 +1,37 @@
 import styles from './Users.module.css';
 import User from "./User/User";
+import axios from "axios";
 
 export const Users = ({users, onFollow, setUsers}) => {
-    if(!users.length) {
-        setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://allya.ru/wp-content/uploads/2020/03/bMM09Rir7LFz6ZnofHmCnfENUZd.jpg',
-                isFollow: false,
-                name: 'Dmitry',
-                status: "Let's rave",
-                location: {
-                    city: 'Chelsea',
-                    country: 'UK',
-                },
-            },
-            {
-                id: 2,
-                photoUrl: 'https://allya.ru/wp-content/uploads/2020/03/bMM09Rir7LFz6ZnofHmCnfENUZd.jpg',
-                isFollow: false,
-                name: 'Lisa',
-                status: "Let's rave",
-                location: {
-                    city: 'Saint-Petersburg',
-                    country: 'Russia',
-                },
-            },
-            {
-                id: 3,
-                photoUrl: 'https://allya.ru/wp-content/uploads/2020/03/bMM09Rir7LFz6ZnofHmCnfENUZd.jpg',
-                isFollow: false,
-                name: 'Mia',
-                status: "Let's rave",
-                location: {
-                    city: 'Sydney',
-                    country: 'Australia',
-                },
-            },
-        ])
+
+    const adaptUserToClientMethod = (user) => {
+        const adaptedUser = {
+            ...user,
+            isFollow: user.followed,
+        };
+        delete adaptedUser.followed;
+        return adaptedUser;
+    };
+
+    const adaptUserToClient = (usersData) => {
+        return usersData.map(adaptUserToClientMethod);
+    };
+
+    if (!users.length) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(({data}) => {
+                setUsers(adaptUserToClient(data.items));
+            })
     }
 
-
     const usersElements = users
-        .map(({name, id, isFollow, photoUrl}) => <User name={name}
-                                                       isFollow={isFollow}
-                                                       key={id}
-                                                       id={id}
-                                                       onFollow={onFollow}
-                                                       photoUrl={photoUrl}/>)
+        .map(({name, id, isFollow, photoUrl, photos}) => <User name={name}
+                                                               isFollow={isFollow}
+                                                               key={id}
+                                                               id={id}
+                                                               photos={photos}
+                                                               onFollow={onFollow}
+                                                               photoUrl={photoUrl}/>)
     return (
         <section className={styles._}>
             <h1>Users</h1>
