5c5902e4c726d606a5a31c4f9c2ad63398a2b2a4
drMonro <Bestpower13>
2021-08-14 23:34:04 +0700

    React JS 77 Использую yup для формы валидации публикации поста

--- app/src/components/Profile/PostSubmit/PostSubmit.jsx
+++ app/src/components/Profile/PostSubmit/PostSubmit.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import * as Yup from 'yup';
 import {Field, withFormik} from "formik";
 
 const PostSubmit = ({submitPost}) => {
-    const PostSubmitForm = ({handleSubmit}) => {
+    const PostSubmitForm = ({errors, touched, handleSubmit}) => {
+        console.log(errors)
         return (
             <form onSubmit={handleSubmit}>
                 <Field as="textarea" name="newPostText" />
+                {errors.newPostText && touched.newPostText &&<div id="feedback">{errors.newPostText}</div>}
                 <button type="submit">Send</button>
             </form>
         )
     };
 
+    const validationSchema = Yup.object().shape({
+        newPostText: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+    });
+
     const PostSubmitFormik = withFormik({
         mapPropsToValues: () => ({ newPostText:'' }),
         handleSubmit: submitPost,
         displayName: 'PostSubmitForm',
+        validationSchema
     })(PostSubmitForm);
 
     return (
--- app/src/components/Users/User/User.jsx
+++ app/src/components/Users/User/User.jsx
@@ -1,6 +1,7 @@
 import styles from './User.module.css';
 import userAvatar from '../../../assets/images/user-avatar.jpg'
 import {NavLink} from "react-router-dom";
+import {withFormik} from "formik";
 
 export const User = ({isFollow, name, onFollow, id, photos, inFollowingUsers}) => {
     const followUser = () => {
@@ -15,6 +16,19 @@ export const User = ({isFollow, name, onFollow, id, photos, inFollowingUsers}) =
         return !!photo ? photo : userAvatar;
     };
 
+    const FollowSubmitForm = ({handleSubmit}) => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <button type="submit" disabled={inFollowingUsers.some(followedId => followedId === id)}>{getFollowTitle()}</button>
+            </form>
+        )
+    };
+
+    const FollowSubmitFormik = withFormik({
+        handleSubmit: followUser,
+        displayName: 'FollowSubmitForm',
+    })(FollowSubmitForm);
+
 
     return (
         <li>
@@ -22,8 +36,9 @@ export const User = ({isFollow, name, onFollow, id, photos, inFollowingUsers}) =
             <NavLink to={`/profile/${id}`}>
                 <p><img className={styles._avatar} src={isUrl(photos.small)} width="90px" height="90px" alt=""/></p>
             </NavLink>
-            <button onClick={followUser} disabled={inFollowingUsers.some(followedId => followedId === id )}>{getFollowTitle()}</button>
+            <FollowSubmitFormik/>
         </li>)
 }
 
 export default User;
+
